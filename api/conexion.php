8d0c8aed7178274ce329778eb7c0279c5c2d0dc7
Diod <49924652+diodZo@users.noreply.github.com>
2024-01-22 11:15:33 -0300

    Update conexion.php
    conectioon

--- api/conexion.php
+++ api/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "solucel_solucel") or
+$conexion = mysqli_connect("u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	", "khgmzsgeteb564b9", "c2ofl6cy0ksn7wuz", "w4qrrws0n802w531") or
 die("Problemas con la conexión");
 
  ?>
