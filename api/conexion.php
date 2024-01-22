ec4761b78e9cfcfaefd55b1705c27256b461cd83
Diod <49924652+diodZo@users.noreply.github.com>
2024-01-22 12:17:37 -0300

    Update conexion.php

--- api/conexion.php
+++ api/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	", "khgmzsgeteb564b9", "c2ofl6cy0ksn7wuz", "w4qrrws0n802w531") or
+$conexion = mysqli_connect("db4free.net", "tulon2023", "tulac4rnud4.", "tulacarnuda") or
 die("Problemas con la conexión");
 
  ?>
